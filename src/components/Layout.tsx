4c97ca62ca694875b2e7c672c8de71584e3fe413
Kit Allen <kitallen23@gmail.com>
2025-06-10 18:09:09 +1000

    Feat: updated background textures, added css pre-processing / modules, started building out the header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,42 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+import styles from "./Layout.module.scss";
+import { Button } from "@/components/ui/button";
 
 export default function Layout() {
     return (
         <>
-            <header className="h-8 bg-primary grid grid-cols-1 items-center">
-                <div className="title text-lg flex items-center text-primary-foreground px-2">
-                    grail.
+            <header
+                className={`${styles.header} h-8 bg-primary text-primary-foreground flex items-center justify-between fixed top-0 right-0 w-full flex-nowrap`}
+            >
+                <div className="flex gap-4 items-center flex-nowrap">
+                    <div
+                        className={`${styles.title} text-lg flex items-center text-primary-foreground px-2 flex-nowrap`}
+                    >
+                        Holy Grail
+                    </div>
+
+                    <nav>
+                        <Button
+                            asChild
+                            variant="link"
+                            className="text-primary-foreground font-bold"
+                            size="sm"
+                        >
+                            <Link to="/">grail</Link>
+                        </Button>
+                        <Button
+                            asChild
+                            variant="link"
+                            className="text-primary-foreground font-bold"
+                            size="sm"
+                        >
+                            <Link to="/">item index</Link>
+                        </Button>
+                    </nav>
                 </div>
             </header>
 
-            <main>
+            <main className="pt-8">
                 <Outlet />
             </main>
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Check } from "lucide-react";
 const Home = () => {
     return (
         <div>
-            <div>Hello, world!</div>
+            <div className="font-bold">Hello, World!</div>
             <div className="flex gap-4">
                 <Button>
                     <div className="flex flex-nowrap gap-x-1 items-center">
@@ -17,7 +17,18 @@ const Home = () => {
                 <Button size="icon">
                     <Check />
                 </Button>
-                <p className="text-muted-foreground">Some muted text</p>
+            </div>
+            <div className="text-lg">This is a test of the new font.</div>
+            <div className="text-xl">abcdefghijklmnopqrstuvwxyz</div>
+            <div className="text-xl">ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
+            <div className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+                faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+                mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+                urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+                egestas. Iaculis massa nisl malesuada lacinia integer nunc
+                posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+                Ad litora torquent per conubia nostra inceptos himenaeos.
             </div>
         </div>
     );
