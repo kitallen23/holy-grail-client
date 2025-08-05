75e525755902d0a10c31d18d31f39daa52e80871
Kit Allen <kitallen23@gmail.com>
2025-08-05 15:28:36 +1000

    chore: bump version to 0.2.0
    Update APP_VERSION constant to reflect minor version increment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const SEARCH_DEBOUNCE_DELAY = 400;
 export const API_CALL_DEBOUNCE_DELAY = 500;
 export const APP_TITLE = "Holy Grail";
-export const APP_VERSION = "0.1.0";
+export const APP_VERSION = "0.2.0";
