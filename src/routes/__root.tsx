4db8cee022dc520c47202abe8d7eb6038e272a80
Kit Allen <kitallen23@gmail.com>
2025-08-05 13:14:12 +1000

    feat: add new title config via tanstack router, removed from index.html

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,10 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import Header from "@/components/layout/Header";
 import { useSearchBar, useSearchDebounceManager } from "@/stores/useSearchStore";
 import LoginModal from "@/components/LoginModal";
 import { Toaster } from "@/components/ui/sonner";
 import ItemDialog from "@/components/ItemTooltip/ItemDialog";
+import { APP_TITLE } from "@/lib/constants";
 
 // import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -14,6 +15,7 @@ const RootLayout = () => {
 
     return (
         <>
+            <HeadContent />
             <Header />
             <main
                 className={`px-2 sm:px-4 mx-auto w-full max-w-4xl h-dvh ${isVisible ? "pt-21" : "pt-8"}`}
@@ -33,4 +35,15 @@ const RootLayout = () => {
 
 export const Route = createRootRoute({
     component: RootLayout,
+    head: () => ({
+        meta: [
+            {
+                name: "Holy Grail",
+                content: "Diablo 2 Holy Grail item tracker",
+            },
+            {
+                title: APP_TITLE,
+            },
+        ],
+    }),
 });
